6ae223b356e6ecaebada47fe30558ca9fc4c9e46
O'Shaughnessy Evans <shaug@niftyisland.io>
2022-03-24 13:50:44 -0700

    add support for passing Unity username and password if given
    2019.x manual doesn't indicate they're required
    - https://docs.unity3d.com/2019.3/Documentation/Manual/CommandLineArguments.html
    2022.x manual shows they should be passed
    - https://docs.unity3d.com/2022.2/Documentation/Manual/ManagingYourUnityLicense.html
    
    also:
    * tell git to ignore vim swap files
    * add `-nographics` arg
    * remove check for UNITY_SERIAL -- the docs don't say anything about using it
      trying to pass it along with -username and -password caused this error:
    
          [Licensing::Module] Error: '-serial' *and* '-returnLicense' commands cannot be used simultaneously

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-if [[ -n "$UNITY_SERIAL" ]]; then
-  #
-  # PROFESSIONAL (SERIAL) LICENSE MODE
-  #
-  # This will return the license that is currently in use.
-  #
-  unity-editor \
-    -logFile /dev/stdout \
-    -quit \
-    -returnlicense
-else
-  echo "No UNITY_SERIAL detected! No license was returned."
+#
+# PROFESSIONAL (SERIAL) LICENSE MODE
+#
+# This will return the license that is currently in use.
+#
+# See: https://docs.unity3d.com/Manual/ManagingYourUnityLicense.html
+#
+
+UNITY_ARGS=""
+if [[ -n "$UNITY_EMAIL" ]]; then
+    UNITY_ARGS+=" -username \"$UNITY_EMAIL\""
 fi
+
+if [[ -n "$UNITY_PASSWORD" ]]; then
+    UNITY_ARGS+=" -password \"$UNITY_PASSWORD\""
+fi
+
+echo "unity-return-license: Returning Unity license"
+unity-editor \
+  -logFile /dev/stdout \
+  -quit \
+  -batchmode \
+  -nographics \
+  -returnlicense $UNITY_ARGS
